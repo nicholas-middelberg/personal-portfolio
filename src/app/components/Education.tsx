401afe1ed377ac930e97e8a7bc0373a588b6e916
nicholas-middelberg <102709066+nicholas-middelberg@users.noreply.github.com>
2025-03-07 17:12:33 -0500

    start working on education section

--- src/app/components/Education.tsx
+++ src/app/components/Education.tsx
@@ -1,8 +1,14 @@
 export default function Education() {
     return (
-      <section id="education">
-        <h2>About Me</h2>
-        <p>This is my bio...</p>
+      <section id="education" className="section">
+        <h2 className="section-title">Education</h2>
+        <div className="education-section">
+          <p className="date-range">09/21 - 06/25</p>
+          <div className="education-content">
+            <h3 className="education-title">Dartmouth College</h3>
+            <p className="education-text">Bachelor of Science in Computer Science</p>
+          </div>
+        </div>
       </section>
     );
   }
